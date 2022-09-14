20bb3e10aeac96df1fce1ea1181a9045993de68d
amberchiu <amber.chiu@lndata.com>
2022-09-14 23:42:39 +0800

    add functions

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const getTip = (value) =>{
- const bill= document.getElementById('bill').value;
- const result = (value / 100) * bill
- console.log(result);
+ const bill= Number(document.getElementById('bill').value);
+ const ppl = Number(document.getElementById('ppl').value);
 
+ //tips
+ let result = 0;
+ if(ppl){
+   result = ((value / 100) * bill) / ppl;
+ }else{
+   result = (value / 100) * bill;
+ }
  document.getElementById('tip-amount').innerHTML = result
+
+ //total
+ let total = 0; 
+ ppl ? total= ((bill/ppl) + result) : total= Number(bill)+result;
+ document.getElementById('ppl-amount').innerHTML = total;
 }
 
+const resetBtn = () =>{
+ document.getElementById('bill').value = 0;
+ document.getElementById('ppl').value = 0;
+ document.getElementById('tip-amount').innerHTML = 0;
+ document.getElementById('ppl-amount').innerHTML = 0;
+}
